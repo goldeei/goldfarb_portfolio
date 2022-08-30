83e81c3f03d84992998fea6c3dfd1671cc6ad2aa
goldeei <jake.goldiee@gmail.com>
2022-08-30 15:33:50 -0400

    Basic modal/animation structure

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,33 +1,62 @@
 import React, { CSSProperties, useState } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { BiMessageSquareDetail } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
 
-//import "./Modal.css";
+import "./modal.css";
 
 const open = {
 	position: "fixed",
 	top: "25%",
-	width: "50%",
+	left: 0,
+	right: 0,
+	margin: "auto",
+	padding: "1rem",
+	width: "80vw",
 	height: "50%",
+	maxWidth: "500px",
+	maxHeight: "500px",
 	backgroundColor: "white",
+	borderRadius: "0.5rem",
+	zIndex: 1001
 };
 
-export default function Modal({ closed }) {
+const closed = {
+	position: "initial",
+	width: "initial",
+	borderRadius: "initial",
+	zIndex: 0
+}
+
+export default function Modal() {
 	const [isOpen, setOpen] = useState(false);
 
 	return (
-		<motion.div style={isOpen ? open : closed} layout>
-			{!isOpen && (
-				<div onPointerDown={() => setOpen(!isOpen)}>
-					<BiMessageSquareDetail />
-				</div>
-			)}
-			{isOpen && (
-				<div onPointerDown={() => setOpen(!isOpen)}>
-					<AiOutlineClose />
-				</div>
-			)}
-		</motion.div>
+		<>
+			<AnimatePresence>
+			{isOpen && 
+				<motion.div 
+					initial={{ opacity: 0 }}
+					whileInView={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className="modal-backdrop"
+				/>
+			}
+			</AnimatePresence>
+			<AnimatePresence>
+				<motion.div className="modal-container" style={isOpen ? open : closed} layout>
+					{!isOpen && (
+						<motion.span layout onPointerDown={() => setOpen(!isOpen)}>
+							<BiMessageSquareDetail />
+						</motion.span>
+					)}
+					{isOpen && (
+						<motion.div layout className="modal-icon__close" onPointerDown={() => setOpen(!isOpen)}>
+							<AiOutlineClose />
+						</motion.div>
+					)}
+				</motion.div>
+			</AnimatePresence>
+		</>
 	);
 }
