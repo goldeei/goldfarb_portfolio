4b2f5f73db1f6103e024eaf9dc2d1930880b5e83
goldeei <jake.goldiee@gmail.com>
2022-09-01 16:35:17 -0400

    Removes test modal

--- src/components/landing-page/LandingPage.jsx
+++ src/components/landing-page/LandingPage.jsx
@@ -1,16 +1,13 @@
 import { motion } from "framer-motion";
 import Modal from "../modal/Modal";
+import "../../App.css";
 
-const style = {
-	width: "200px",
-	backgroundColor: "blue",
-};
+const style = {};
 
 export default function LandingPage() {
 	return (
 		<div className="fullscreen center-children">
 			<div className="title">Jake Goldfarb</div>
-			<Modal closed={style} />
 		</div>
 	);
 }
