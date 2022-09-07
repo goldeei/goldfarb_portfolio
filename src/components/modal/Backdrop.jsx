c4549c1d05240d34b91bb75273bd6e8f80d66073
goldeei <jake.goldiee@gmail.com>
2022-09-07 16:12:35 -0400

    Modal backdrop animations

--- src/components/modal/Backdrop.jsx
+++ src/components/modal/Backdrop.jsx
@@ -1,12 +1,25 @@
 import { motion } from "framer-motion";
 
+const variants = {
+	visible: {
+		opacity: 1,
+	},
+	hidden: {
+		opacity: 0,
+	},
+};
+
 export const Backdrop = ({ children, onClick }) => {
 	return (
 		<motion.div
-			initial={{ opacity: 0 }}
-			whileInView={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
 			className="modal-backdrop"
+			initial="hidden"
+			animate="visible"
+			exit="hidden"
+			transition={{
+				duration: 0.1,
+			}}
+			variants={variants}
 			onClick={onClick}
 		>
 			{children}
