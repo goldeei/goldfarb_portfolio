87651ff3c7e06549d24ad813c035cec72ec3aa04
goldeei <jake.goldiee@gmail.com>
2022-09-07 16:10:59 -0400

    Cleanup close button icons

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import { AiOutlineClose } from "react-icons/ai";
+import { IoClose } from "react-icons/io5";
 import "./Buttons.css";
 
 // Returns a close button that animates red on hover, scale on click
@@ -7,21 +7,20 @@ import "./Buttons.css";
 export const CloseButton = ({ style, handleClose }) => {
 	return (
 		<motion.button
-			layout
 			style={style}
 			// var --scale allows for scale to be set by parent while still animating for reusability
 			initial={{ "--scale": style.scale }}
-			whileTap={{ "--scale": style.scale * 0.9 }}
+			whileTap={{ "--scale": style.scale * 0.8 }}
 			whileHover={{
 				color: "rgb(255,0,0)",
 			}}
 			onClick={handleClose}
+			transition={{
+				duration: 0.2,
+				ease: "easeInOut",
+			}}
 		>
-			<AiOutlineClose
-				layout
-				style={{ transform: "scale(var(--scale))" }}
-				as={motion.svg}
-			/>
+			<IoClose style={{ transform: "scale(var(--scale))" }} as={motion.svg} />
 		</motion.button>
 	);
 };
