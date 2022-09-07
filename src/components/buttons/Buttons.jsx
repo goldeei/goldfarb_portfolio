18ca0cf79a2f1253361f694a9b04743012a7d716
goldeei <jake.goldiee@gmail.com>
2022-09-07 18:11:13 -0400

    Update Buttons.jsx

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -8,13 +8,13 @@ export const CloseButton = ({ style, handleClose }) => {
 	return (
 		<motion.button
 			style={style}
+			onClick={handleClose}
 			// var --scale allows for scale to be set by parent while still animating for reusability
 			initial={{ "--scale": style.scale }}
 			whileTap={{ "--scale": style.scale * 0.8 }}
 			whileHover={{
 				color: "rgb(255,0,0)",
 			}}
-			onClick={handleClose}
 			transition={{
 				duration: 0.2,
 				ease: "easeInOut",
