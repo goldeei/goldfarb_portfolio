7b30583846d3cdf9e40cecba193c2678d79ae414
goldeei <jake.goldiee@gmail.com>
2022-09-13 17:36:01 -0400

    Comment randBetween

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -1,3 +1,8 @@
-export const randBetween = (max, min) => {
+/**
+ * @param {float} min min boundary
+ * @param {float} max max boundary
+ * @returns a random number between min and max
+ */
+export const randBetween = (min, max) => {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
