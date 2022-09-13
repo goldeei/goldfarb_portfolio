3876e71b54a7fd5a017fb30ba6b342535efe1d5a
goldeei <jake.goldiee@gmail.com>
2022-09-13 19:00:44 -0400

    Add comments

--- src/components/styling/Layout.jsx
+++ src/components/styling/Layout.jsx
@@ -1,7 +1,17 @@
-//Sets default max width
+//TODO Move/refactor to GlobalStyling
+
+//Default max width
 export const defMax = "min(95vw, 1200px)";
 
-//Returns z-index depending on element's purpose
+/**
+ * @param level the related z-index string
+ * * "background" = -1
+ * * "content" = 1
+ * * "control" = 2
+ * * "overlay" = 3
+ * * null = 0
+ * @returns the associated z-index
+ */
 export const zIndex = (level) => {
 	if (level === "background") return "-1";
 	if (level === "content") return "1";
