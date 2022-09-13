8aeb05a87e568e07211f0fb5ffbf184d75ebd135
goldeei <jake.goldiee@gmail.com>
2022-09-13 17:51:18 -0400

    Add error checks, param types

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -1,8 +1,12 @@
 /**
- * @param {float} min min boundary
- * @param {float} max max boundary
+ * @param {Number} min min boundary
+ * @param {Number} max max boundary
  * @returns a random number between min and max
  */
 export const randBetween = (min, max) => {
+	if (!min) return console.error("Missing min value");
+	if (!max) return console.error("Missing max value");
+	if (typeof min !== Number || typeof max !== Number)
+		return console.error("Params must be numbers");
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
