d529564b1e69c137876a2197fbee35cd0c0dcdbf
goldeei <jake.goldiee@gmail.com>
2022-09-13 17:55:38 -0400

    Remove more type checks
    Will move to tsx when/if necessary

--- src/components/Utilities.jsx
+++ src/components/Utilities.jsx
@@ -6,7 +6,5 @@
 export const randBetween = (min, max) => {
 	if (!min) return console.error("Missing min value");
 	if (!max) return console.error("Missing max value");
-	if (typeof min !== Number || typeof max !== Number)
-		return console.error("Params must be numbers");
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 };
