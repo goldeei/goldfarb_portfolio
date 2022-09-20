24e9329be76ede4d4d165aa5f93500679bec1e7e
goldeei <jake.goldiee@gmail.com>
2022-09-19 20:59:51 -0400

    AboutMe content

---
+++ src/components/about-me/Content.jsx
@@ -0,0 +1,18 @@
+export const title = "Hi I'm Jake Goldfarb";
+
+export const subtitle =
+	"I'm a UI Designer, Frontend Developer, and Data Analyst";
+
+export const body =
+	"I enjoy using creative approaches to creating web apps and data visualizations that make them enjoyable, approachable, and accessible using modern Javascript frameworks and libraries.";
+
+export const highlighted = [
+	"Jake",
+	"Goldfarb",
+	"UI",
+	"Designer",
+	"Frontend",
+	"Developer",
+	"Data",
+	"Analyst",
+];
