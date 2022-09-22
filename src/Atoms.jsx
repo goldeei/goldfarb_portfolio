bd1115c5e88c4f8fe9b7ec2d19898587c2c06328
goldeei <jake.goldiee@gmail.com>
2022-09-22 15:50:25 -0400

    Remove default hash setting from atom

--- src/Atoms.jsx
+++ src/Atoms.jsx
@@ -1,9 +1,6 @@
 import { atom } from "recoil";
 
-//Returns current hash if it exists, else return default
-const activeDefault = () =>
-	location.hash ? location.hash.replace("#", "") : "about-me";
 export const activeSectionState = atom({
 	key: "activeSection",
-	default: activeDefault(),
+	default: "about-me",
 });
