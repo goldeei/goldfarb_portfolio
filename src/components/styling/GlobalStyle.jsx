e6c0c12277834f7cefcf39c18c085ad84859791d
goldeei <jake.goldiee@gmail.com>
2022-10-06 14:50:27 -0400

    Transfer some of App.css root to GlobalStyle

--- src/components/styling/GlobalStyle.jsx
+++ src/components/styling/GlobalStyle.jsx
@@ -7,6 +7,27 @@ const fullscreen = css`
 `;
 
 export const GlobalStyle = createGlobalStyle`
+    :root {
+        font-family: Inter, Avenir, Helvetica, Arial, sans-serif, KleeOne,
+		Noto Sans JP;
+        font-size: 20px;
+        line-height: 24px;
+        font-weight: 400;
+        color: ${(props) => props.theme.colors.mainText};
+        background-color: ${(props) => props.theme.colors.background};
+        font-synthesis: none;
+        text-rendering: optimizeLegibility;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        -webkit-text-size-adjust: 100%;
+        scroll-behavior: smooth;
+    }
+    h1, p {
+        color: ${(props) => props.theme.colors.mainText};
+    }
+    h2, h3, h4, h5 {
+        color: ${(props) => props.theme.colors.subText};
+    }
     section {
         position: relative;
         display: flex;
@@ -16,6 +37,11 @@ export const GlobalStyle = createGlobalStyle`
         };
         > div {
             height: fit-content;
+	        padding: 0.75rem;
         }
     };
+    * {
+        transition-property: background-color, color;
+        transition-duration: 0.2s;
+        }
 `;
