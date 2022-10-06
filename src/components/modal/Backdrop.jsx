0dc233da02df4731c558df5b0873973e560f8bb0
goldeei <jake.goldiee@gmail.com>
2022-10-06 19:23:07 -0400

    Correct centering

--- src/components/modal/Backdrop.jsx
+++ src/components/modal/Backdrop.jsx
@@ -33,8 +33,8 @@ const StyledBackdrop = styled(motion.div)`
 	position: fixed;
 	left: 0;
 	top: 0;
-	width: 100vw;
-	height: 100vh;
+	width: 100%;
+	height: 100%;
 	display: flex;
 	justify-content: center;
 	align-items: center;
