8cd045bb0d0005c7a41e5b00a52f1832bd29783b
goldeei <jake.goldiee@gmail.com>
2022-10-14 11:06:08 -0400

    Update navbar styles

--- src/components/navbar/Styled.jsx
+++ src/components/navbar/Styled.jsx
@@ -1,27 +1,32 @@
 import { motion } from "framer-motion";
 
 import styled from "styled-components";
-import * as Layout from "../styling/Layout";
+import * as layout from "../styling/Layout";
 
 export const Nav = styled.nav`
 	position: fixed;
 	left: 0;
 	top: 0;
-	z-index: ${Layout.zIndex("control")};
 	width: 100%;
+	z-index: ${layout.zIndex("navbar")};
 `;
 
-export const NavbarNav = styled.ul`
-	margin: 0 auto;
-	padding: 0;
-	max-width: ${Layout.defMax};
-	list-style-type: none;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	background-color: white;
-	box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;
-`;
+export const NavbarNav = styled.ul(
+	({ theme }) => `
+		position: relative;
+		background-color: ${theme.colors.primary};
+		margin: 0.2rem auto;
+		padding: 0;
+		height: 2.5rem;
+		border-radius: 0.15rem;
+		max-width: ${layout.defMax};
+		list-style-type: none;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		box-shadow: ${theme.frontShadow};
+	`
+);
 
 export const NavbarItem = styled(motion.li)`
 	position: relative;
@@ -29,21 +34,23 @@ export const NavbarItem = styled(motion.li)`
 	height: 100%;
 	display: flex;
 	justify-content: center;
-	& > a {
+	align-items: center;
+	/* Links */
+	> a {
 		font-size: clamp(0.6rem, 2vw, 0.8rem);
-		padding: 0.6rem 1.5rem;
 		text-decoration: none;
 		text-align: center;
 		flex: 1;
-		color: rgb(0, 0, 0);
+		cursor: pointer;
 	}
 `;
 
+// Indicator
 export const Underline = styled(motion.div)`
 	position: absolute;
 	bottom: 0.25rem;
 	width: 60%;
 	height: clamp(0.2rem, 0.4vw, 0.25rem);
-	background: #04d9ff;
+	background: ${(props) => props.theme.colors.highlight};
 	border-radius: 8px;
 `;
