416c64f3577391c6cb3bc6a867122f2d515ed757
goldeei <jake.goldiee@gmail.com>
2022-10-14 11:06:35 -0400

    Add site control styling on mobile

--- src/components/controls/Controls.jsx
+++ src/components/controls/Controls.jsx
@@ -10,25 +10,20 @@ const ControlContainer = styled.div`
 	margin: 0 1rem;
 	width: 100%;
 	height: 100;
-	z-index: ${layout.zIndex("site-controls")};
 	display: flex;
 	justify-content: right;
-	> button {
-		border-radius: 0.25rem;
-		box-shadow: rgba(17, 17, 26, 0.1) 0px 1px 0px 0px;
-		z-index: 2;
-	}
 	> button:not(:last-child) {
 		margin-right: 0.5rem;
-	}
-	> button > svg {
-		padding: 0.25rem 0.3rem;
-		scale: 1.95;
 	}
 	@media screen and (max-width: 500px) {
 		justify-content: center;
 		position: fixed;
-		bottom: 1rem;
+		bottom: 0.5rem;
+		background-color: ${(props) => props.theme.colors.primary};
+		width: 80%;
+		padding: 0.5rem;
+		border-radius: 0.25rem;
+		box-shadow: ${(props) => props.theme.frontShadow};
 	}
 `;
 
