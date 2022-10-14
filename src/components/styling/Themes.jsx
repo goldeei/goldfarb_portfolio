e36d76ddb22cde6eab43bdcbf109c113ce48032a
goldeei <jake.goldiee@gmail.com>
2022-10-14 11:03:50 -0400

    Additional theme styling

--- src/components/styling/Themes.jsx
+++ src/components/styling/Themes.jsx
@@ -4,7 +4,13 @@ const colors = {
 };
 
 export const base = {
-	containerDropShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px;",
+	frontShadow:
+		"rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;",
+	containerDropShadow:
+		"rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px;",
+	buttonShadow:
+		"rgba(9, 30, 66, 0.1) 0px 4px 4px -2px, rgba(9, 30, 66, 0.03) 0px 0px 0px 1px;",
+	warning: "rgb(255, 0, 0)",
 };
 export const light = {
 	primary: "white",
@@ -13,6 +19,7 @@ export const light = {
 	subText: "grey",
 	highlightText: colors.midBlue,
 	highlight: colors.neonBlue,
+	hover: "lightgrey",
 };
 export const dark = {
 	primary: "#404040",
@@ -21,4 +28,5 @@ export const dark = {
 	subText: "lightgrey",
 	highlightText: colors.neonBlue,
 	highlight: colors.neonBlue,
+	hover: "lightgrey",
 };
