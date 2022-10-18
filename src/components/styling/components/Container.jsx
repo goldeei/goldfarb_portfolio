0bd0b9460f7f4c01340f3f3ed8777a7efc2bdf65
goldeei <jake.goldiee@gmail.com>
2022-10-17 21:03:45 -0400

    Change container to pure layout div

--- src/components/styling/components/Container.jsx
+++ src/components/styling/components/Container.jsx
@@ -2,29 +2,14 @@ import styled, { css } from "styled-components";
 
 const Container = styled.div.attrs((props) => ({
 	width: props.width || "max(500px, 50%)",
+	height: props.height || "80%",
 }))`
 	${(props) => {
-		const colors = props.theme.colors;
 		return css`
-			> h1 {
-				color: ${colors.mainText};
-				background-color: ${colors.primary};
-				padding: 1rem;
-				margin: 0;
-				border-radius: 0.2rem;
-				box-shadow: ${props.theme.containerDropShadow};
-			}
+			max-width: ${props.width};
+			height: ${props.height};
 		`;
 	}}
-	width: ${(props) => props.width};
-	height: 80%;
-	position: relative;
-	display: flex;
-	flex-direction: column;
-	align-self: center;
-	transition-property: height, width;
-	transition-duration: 0.25s;
-	border-radius: 0.2rem;
 `;
 
 export default Container;
