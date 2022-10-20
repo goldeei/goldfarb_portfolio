374d249b7f251f27dd226f8006f46cd117b0f820
goldeei <jake.goldiee@gmail.com>
2022-10-19 21:17:32 -0400

    Move models

--- src/r3f/character/Character.jsx
+++ src/r3f/character/Character.jsx
@@ -7,7 +7,7 @@ function Character() {
 	const main = useRef(!null);
 	useFrame((state, delta) => {
 		main.current.position.y +=
-			Math.sin(state.clock.getElapsedTime() * 1) * 0.002;
+			Math.sin(state.clock.getElapsedTime() * 1) * 0.0005;
 		main.current.rotation.x =
 			Math.sin(state.clock.getElapsedTime() * 1) * 0.005;
 		main.current.rotation.y =
@@ -15,9 +15,9 @@ function Character() {
 	});
 
 	return (
-		<group ref={main} scale={0.9} position={[2.5, 0, 0]}>
+		<group ref={main} scale={0.9} position={[2.5, -0.4, 0]}>
 			<Bottom dimensions={[1.15, 1]} scale={4.5} position={[0.1, -1.25, 0]} />
-			<Grass dimensions={[1.32, 0.9]} scale={4} position={[0.05, 0, 0.1]} />
+			<Grass dimensions={[1.32, 0.9]} scale={4} position={[0.05, -0.1, 0.1]} />
 			<Guy scale={1.75} dimensions={[1, 1.2]} position={[0, 1.1, 0.125]} />
 		</group>
 	);
